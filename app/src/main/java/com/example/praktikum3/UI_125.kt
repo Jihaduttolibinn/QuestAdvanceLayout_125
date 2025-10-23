cf6c3f7cf08a8c2b40e375ad1215370c6ccb61d5
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-23 15:30:03 +0700

    Menambahkan fun ActivasPertama

--- app/src/main/java/com/example/praktikum3/UI_125.kt
+++ app/src/main/java/com/example/praktikum3/UI_125.kt
@@ -1,2 +1,21 @@
 package com.example.praktikum3
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import java.lang.reflect.Modifier
+
+
+@Composable
+fun ActivasPertama(modifier: Modifier){
+    Column {
+        modifier = modifier
+            .padding(top = 100.dp)
+            .fillMaxSize(),
+        horizontalAligment = Alignment.CenterHorizontally
+        ){
