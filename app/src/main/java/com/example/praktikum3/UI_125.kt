00d92b6877454b526bedb889b1292f644349f16c
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-23 16:00:28 +0700

    Menambahkan Spacer

--- app/src/main/java/com/example/praktikum3/UI_125.kt
+++ app/src/main/java/com/example/praktikum3/UI_125.kt
@@ -2,32 +2,39 @@ package com.example.praktikum3
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import java.lang.reflect.Modifier
+import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun ActivasPertama(modifier: Modifier){
-    Column {
-        modifier = modifier
-            .padding(top = 100.dp)
+fun ActivasPertama(modifier: Modifier) {
+    Column (
+        modifier = Modifier.padding(top = 100.dp)
             .fillMaxSize(),
-        horizontalAligment = Alignment.CenterHorizontally
-        ){
-        text(
-            text = stringResource(),
-            fontSize = 35.dp,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Text(
+            stringResource(R.string.Prodi),
+            fontSize = 35.sp,
             fontWeight = FontWeight.Bold
         )
         Text(
-            text = stringResource(id = R.string.univ),
+            stringResource(R.string.univ),
             fontSize = 22.sp
         )
+        Spacer(modifier = modifier.height(height = 25.dp))
 
+        )
 
     }
+}
