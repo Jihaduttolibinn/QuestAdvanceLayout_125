5a5ccf17858ea20d2e4f0be57c26a9583ccae869
jihaduttolibinn <jihaduttolibin02@gmail.com>
2025-10-23 16:23:54 +0700

    Menambahkan poto bapakkumis

--- app/src/main/java/com/example/praktikum3/UI_125.kt
+++ app/src/main/java/com/example/praktikum3/UI_125.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -52,6 +53,7 @@ fun ActivasPertama(modifier: Modifier) {
                 verticalAlignment = Alignment.CenterVertically,
                 modifier = Modifier.padding(8.dp)
             ){
+                val gambar = painterResource(id = R.drawable.bapakkumis)
 
             }
 
